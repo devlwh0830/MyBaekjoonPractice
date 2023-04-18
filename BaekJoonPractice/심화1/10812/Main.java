d7bb33ceeab48869a3178a716f28a593332d8e1e
Lee WonHee <story3798@naver.com>
2023-04-18 15:40:27 +0900

    10812번 해결중

--- BaekJoonPractice/심화1/10812/Main.java
+++ BaekJoonPractice/심화1/10812/Main.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int[] list = {};
+
+        for(int j=0;j<10;j++){
+            list[j] = j+1;
+        }
         
+        for(int i=0;i<b;i++){
+            int x = sc.nextInt(); // 시작
+            int y = sc.nextInt(); // 끝
+            int m = sc.nextInt(); // 중간값
+        }
+
+        sc.close();
     }
 }
